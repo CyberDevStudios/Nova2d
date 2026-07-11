fb75f7a7b9d53965d3c68256604aa2f29358a786
MatFon73 <mateofon78@hotmail.com>
2026-07-10 21:16:47 -0500

    chore: bump version to v0.6.0-pre.1 across codebase and README

--- src/states/menu.lua
+++ src/states/menu.lua
@@ -172,7 +172,7 @@ function State:draw()
     -- Version
     love.graphics.setFont(ensureFont(fontVersion, 11))
     love.graphics.setColor(1, 1, 1, 0.12)
-    love.graphics.printf("v0.5", 0, H - 22, W, "center")
+    love.graphics.printf("v0.6", 0, H - 22, W, "center")
 
     love.graphics.setColor(1, 1, 1)
 end
